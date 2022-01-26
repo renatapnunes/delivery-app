a2f8b8e9bf40e37c16ddbbba5c3bca2e9341bbad
Luiz Henrique <82241841+lzzhenrique@users.noreply.github.com>
2022-01-26 17:32:12 -0300

    [MAIN-GROUP-15] [ADD] [GET ALL PRODUCTS] (#169)
    * ADD: datatest-id object
    
    * ADD: rota para getAllProducts

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const express = require('express');
+// const bodyParser = require('body-parser');
+const root = require('../controllers/router');
+const error = require('../middlewares/err');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/images', express.static(path.resolve(__dirname, '../public')));
+
+app.use(express.json());
+
+app.use('/', root);
+
+app.use(error);
 
 module.exports = app;
