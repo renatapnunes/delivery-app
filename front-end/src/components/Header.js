31f5e9b3748e5dbedda738f4a2f76702ffacd9bf
GMODias <82231635+GMODias@users.noreply.github.com>
2022-02-03 11:27:49 -0300

    [GROUP 15] [ADD: HEADER STYLES] (#585)
    * ADD: styling header and its functions.
    
    * Starting to create 2 different headers.
    
    * ADD: created 2 different header elements: 1 for seller other to costumer.
    
    * Fixing Lint Problem.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,54 +1,16 @@
-import React, { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-import dataTestIds from '../utils/dataTestIds';
+import HeaderCostumer from './HeaderCostumer';
+import HeaderSeller from './HeaderSeller';
 
 function Header() {
-  const { current: { name } } = useRef(JSON.parse(localStorage.getItem('user') || ''));
-  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  function handleChange({ name: buttonName }) {
-    if (buttonName === 'login') localStorage.removeItem('user');
-    return navigate(`/${buttonName}`);
-  }
+  const role = pathname.split('/')[1];
 
-  return (
-    <div style={ { display: 'flex' } }>
-      <div>
-        <button
-          name="customer/products"
-          data-testid={ dataTestIds[11] }
-          type="button"
-          onClick={ (e) => handleChange(e.target) }
-        >
-          PRODUTOS
-        </button>
-      </div>
-      <div>
-        <button
-          name="customer/orders"
-          type="button"
-          onClick={ (e) => handleChange(e.target) }
-          data-testid={ dataTestIds[12] }
-        >
-          PEDIDOS
-        </button>
-      </div>
-      <div
-        data-testid={ dataTestIds[13] }
-      >
-        { name }
-      </div>
-      <button
-        name="login"
-        type="button"
-        onClick={ (e) => handleChange(e.target) }
-        data-testid={ dataTestIds[14] }
-      >
-        Sair
-      </button>
-    </div>
-  );
+  if (role === 'seller') return <HeaderSeller />;
+  return <HeaderCostumer />;
 }
 
 export default Header;
